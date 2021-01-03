9e1020b9a88eb8f8a81c6838bcf592f002a6392a
Sean Pierce <sumler.sean@gmail.com>
2021-01-03 14:10:18 -0800

    Initialize client app

---
+++ stream/index.js
@@ -0,0 +1,8 @@
+// Add this on the top of app.js file
+// next to all imports
+const server = require('./server');
+ 
+// and call run() method at the end
+// file where we start our web server
+ 
+server.run();
